c92866e61d5bd910d16b2316ae7f4bb543513b78
derick189 <vk9018fk@metrostate.edu>
2017-03-18 22:04:32 -0500

    pull upstream

--- Library/LibraryTest.java
+++ Library/LibraryTest.java
@@ -16,24 +16,24 @@ import java.io.File;
  * Created by Kevin on 3/18/2017.
  */
 public class LibraryTest {
-    static Library lib;
+
+    static String currentDir;
 
     @BeforeClass
     public static void setUpClass() {
-        // Initialize the lib object
-
-        lib = new Library();
-        //
-        // Now get the items into the Library:
-        //
-        String currentDir = System.getProperty("user.dir");
-        File file = new File(currentDir + "\\Test.json");
-        lib.addFileDataJson(file);
-        System.out.println(lib.displayItems("Book"));
+        currentDir = System.getProperty("user.dir");
     }
 
     @Test
     public void testCheckOutCheckIn() {
+        // Initialize the lib object
+        Library lib = new Library();
+        //
+        // Now get the items into the Library:
+        //
+        File file = new File(currentDir + "\\Test.json");
+        lib.addFileDataJson(file);
+
         //
         // Checkout an item
         //
@@ -65,18 +65,18 @@ public class LibraryTest {
         //
         // First test for a nonexistent file
         //
-        File file = new File("TestFileDoesNotExist.json");
+        File file = new File(currentDir + "\\TestFileDoesNotExist.json");
         lib.addFileDataJson(file);
         //
         // Now test with a valid file that has three entries that have errors :
         // 1) No ID (ID = "")
         // 2) Invalid type = VHS
         // 3) Name = null (No name field)
-        // It then checks that 8 items were added
+        // It then checks that 4 items were added
         //
-        file = new File("Test.json");
+        file = new File(currentDir + "\\Test.json");
         lib.addFileDataJson(file);
-        assertEquals("Number of items added is incorrect: ", 8, lib.size());
+        assertEquals("Number of items added is incorrect: ", 4, lib.size());
         assertEquals("The Stand", lib.getItem("1adf5").getName());
     }
 
@@ -87,7 +87,7 @@ public class LibraryTest {
         //
         // First test for a nonexistent file
         //
-        File file = new File("TestFileDoesNotExist.xml");
+        File file = new File(currentDir + "\\TestFileDoesNotExist.xml");
         lib.addFileDataXml(file);
         //
         // Now test with a valid file that has three entries that have errors :
@@ -96,7 +96,7 @@ public class LibraryTest {
         // 3) Name = null (No name field)
         // It then checks that 4 items were added
         //
-        file = new File("Test.xml");
+        file = new File(currentDir + "\\Test.xml");
         lib.addFileDataXml(file);
         assertEquals("Number of items added is incorrect: ", 4, lib.size());
     }
@@ -108,9 +108,8 @@ public class LibraryTest {
         //
         // Get the Library data from a file
         //
-        File file = new File("Test.json");
+        File file = new File(currentDir + "\\Test.json");
         lib.addFileDataJson(file);
-        Boolean response = lib.checkOut("48934k", lib);
-        System.out.println(lib.displayItems("cd"));
+        Boolean response = lib.checkOut("48934j", lib);
     }
 }
