85135493ec18f30daa795108dd26e2acb86ca42a
derick189 <derick189@users.noreply.github.com>
2017-03-20 02:28:29 -0500

    Revert "Added enums (#6)"
    This reverts commit 24415dd6272eb05baac13a2e718262bc8c8d3e13.

--- Items/Item.java
+++ Items/Item.java
@@ -7,32 +7,26 @@ import java.util.Calendar;
  * Item class is to be extended by subclasses of each type of Item.
  * It maintains protected attributes common to all types of Items.
  */
+
 public class Item implements Serializable {
 
     protected String id;
     protected String name;
-    protected Type type;
+    protected String type;
     protected boolean available;    // Available in the library -- false = checked out
     protected Calendar dateDue;     // Due date
     protected int checkOutTimeDays; // Number of days that item can be checked out
 
+
     public Item() {
     }
 
-    public Item(String id, String name, Type type) {
+    public Item(String id, String name, String type) {
         this.id = id;
         this.name = name;
         this.type = type;
         this.available = true;
         this.dateDue = Calendar.getInstance();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -43,20 +37,20 @@ public class Item implements Serializable {
         this.name = name;
     }
 
-    public Type getType() {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
         return type;
     }
 
-    public void setType(Type type) {
+    public void setType(String type) {
         this.type = type;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
     }
 
     public Calendar getDateDue() {
@@ -67,6 +61,14 @@ public class Item implements Serializable {
         this.dateDue = dateDue;
     }
 
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
     public int getCheckOutTimeDays() {
         return checkOutTimeDays;
     }
@@ -74,41 +76,22 @@ public class Item implements Serializable {
     public void setCheckOutTimeDays(int checkOutTimeDays) {
         this.checkOutTimeDays = checkOutTimeDays;
     }
-  
-    @Override
-    public String toString()
-    {
-      String message = "Item ID: " + getId() +
-               " -- Type: " + getType() +
-                 " -- Name: " + getName();
-      if(dateDue != null)
-      {
-        message += " -- Due Date: " +
-                 (dateDue.get(Calendar.MONTH) + 1) +
-                 "/" + dateDue.get(Calendar.DAY_OF_MONTH)
-                  + "/" + dateDue.get(Calendar.YEAR) + "\n";
-      }
-      return message;
-    }
-  
-    @Override
-    public String toString()
-    {
-      String message = "Item ID: " + getId() +
-                         " -- Type: " + getType() +
-                           " -- Name: " + getName();
-      if(dateDue != null)
-      {
-        message += " -- Due Date: " +
-                        (dateDue.get(Calendar.MONTH) + 1) +
-                         "/" + dateDue.get(Calendar.DAY_OF_MONTH) +
-                         "/" + dateDue.get(Calendar.YEAR) + "\n";
-      }
 
-      return message;
-    }
-  
-    public enum Type {
-        BOOK, CD, DVD, MAGAZINE;
-    }
+	@Override
+	public String toString()
+	{
+		String message = "Item ID: " + getId() +
+    	                 " -- Type: " + getType() +
+                         " -- Name: " + getName();
+		if(dateDue != null)
+		{
+			message += " -- Due Date: " +
+                      (dateDue.get(Calendar.MONTH) + 1) +
+                       "/" + dateDue.get(Calendar.DAY_OF_MONTH) +
+                       "/" + dateDue.get(Calendar.YEAR) + "\n";
+		}
+		
+		return message;
+	}
+
 }
